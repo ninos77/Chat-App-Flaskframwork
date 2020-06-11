deebc27191eb3cf93383a5f2e4ccffe111c6b290
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-06-11 20:30:27 +0000

    Implement basic views and routing

--- run.py
+++ run.py
@@ -9,4 +9,14 @@ def index():
     return "<h1> Hello there </h2>"
 
 
+@app.route("/<username>")
+def user(username):
+    return "Hi " + username
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return f"{username}: {message}"
+
+
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)

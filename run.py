865514c37467a0b15f89f9563ce898837b51a0aa
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-06-12 21:45:44 +0000

    add new template

--- run.py
+++ run.py
@@ -1,27 +1,31 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 
+now = datetime.now().strftime("%H:%H:%S")
 messages = []
 
 
 def add_message(username, message):
-    messages.append(f"{username}: {message}")
+    message_dict = {"time": now, "username": username, "message": message}
+    messages.append(message_dict)
 
 
-def get_all_messages():
-    return "<br>".join(messages)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1> Hello there </h2>"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    if "username" in session:
+        return redirect(session["username"])
+    return render_template("index.html")
 
 
 @app.route("/<username>")
 def user(username):
-    return f"<h1>Welcome,{username}<h1/>{get_all_messages()}"
+    return f"<h1>Welcome,{username}<h1/> {messages}"
 
 
 @app.route("/<username>/<message>")
